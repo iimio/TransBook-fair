bdd15fd75c3860593d545f46e8e11bd5377c0a3f
hgw <register@shunyahagiwara.com>
2018-09-10 21:14:20 +0900

    2018 の設定を追加

---
+++ src/2018/src/js/app.js
@@ -0,0 +1,24 @@
+console.log('app start! 2018');
+
+import $ from 'jquery';
+import jQuery from 'jquery';
+
+window.$ = $;
+window.jQuery = jQuery;
+
+export default class App {
+
+  /**
+   *
+   */
+  constructor() {
+  }
+
+
+}
+
+$(function () {
+
+  new App();
+
+});
